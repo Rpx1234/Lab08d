41d0fb753652688c7efb6ac29da65400e94d3f6c
Rpx1234 <22777764+Rpx1234@users.noreply.github.com>
2019-01-31 11:25:40 -0600

    Add files via upload

---
+++ Lab08d.java
@@ -0,0 +1,31 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab08d
+{
+ public static void main ( String[] args )
+ {
+  //add test cases
+   TriangleOne test = new TriangleOne("hippo");
+   test.print();
+   
+   test.setWord("abcd");
+   test.print();
+  
+   test.setWord("it");
+   test.print();
+   
+   test.setWord("a");
+   test.print();
+   
+   test.setWord("chicken");
+   test.print();
+  
+  
+  
+ }
+}
